1e393d442d7f555177edf4f1b3a094dc86546325
lucy <lovelylucyscorpse@outlook.com>
2025-10-12 07:30:17 +0000

    ace attorney??

---
+++ games/Ace Attorney/emujs/play.php
@@ -0,0 +1,25 @@
+<html>
+    <head>
+        <style>
+		body{
+			background:#000;
+		}
+		</style>
+			<meta name="robots" content="noindex, nofollow"/>
+    </head>
+    <body>
+      <div id="display" style="width:100%;height:100%;max-width:100%">
+            <div id="game"></div>
+        </div>
+        <script>
+    EJS_player = '#game';
+    EJS_core = 'gba';
+EJS_startOnLoaded = true;
+    EJS_biosUrl = ''; 
+    EJS_gameUrl = '/r/gba/ace-attorney.gba'; 
+    EJS_pathtodata = '4.2.1/data/';
+EJS_backgroundColor = '#000';
+        </script>
+<script src='4.2.1/data/loader.js'></script>
+    </body>
+</html>
